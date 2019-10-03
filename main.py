a3c1344b0c07a00c8e9cf7f86d8ff4369315c8ae
Rei Sato <madoibito80@Macbook.local>
2019-10-04 04:54:01 +0900

    readme

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import check
 import check2
 import pandas as pd
 
-major = 4
+major = int(sys.argv[2])
+csv_file = sys.argv[1]
+
 senko = ["ソフトウェアサイエンス","情報システム","知能情報メディア"][major-2]
 print("専攻 : "+senko+" GB"+str(major))
 
 
-df = pd.read_csv('./data.csv')
+df = pd.read_csv(csv_file)
 df = check.f(df, senko=senko)
 
 df = check2.spots2(df, major=major)
